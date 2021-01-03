0778919b4c8ed968d7a8c8c23d7ad4ba76a2f720
wangcancan <1105133742@qq.com>
2021-01-03 18:12:23 +0800

    邮寄地址

--- src/view/exam/examStep_4.vue
+++ src/view/exam/examStep_4.vue
@@ -2,15 +2,31 @@
   <div class="examstep_4-page">
     <div class="info-form-block">
       <h3>邮寄地址</h3>
-      <div>
+      <div v-if="status== 1">
         <van-field v-model="userInfo.name" label="收件人姓名" placeholder="请输入" />
         <van-field v-model="userInfo.name" label="收件人电话" placeholder="请输入" />
-        <van-field v-model="userInfo.name" label="收件人详细地址" placeholder="请输入" />
+        <van-field v-model="userInfo.name" rows="1" autosize label="收件人详细地址" placeholder="请输入" type="textarea" />
+      </div>
+      <div v-else>
+        <van-field v-model="userInfo.name" label="收件人姓名" placeholder="请输入" readonly />
+        <van-field v-model="userInfo.name" label="收件人电话" placeholder="请输入" readonly />
+        <van-field v-model="userInfo.name" rows="1" autosize label="收件人详细地址" placeholder="请输入" type="textarea"
+          readonly />
       </div>
     </div>
-    <div class="step-btn-group">
-      <van-button type="theme" class="btn">完成</van-button>
-      <!-- <van-button type="theme" class="btn" >返回课程</van-button> -->
+
+    <div class="info-form-block" v-if="status != 1">
+      <h3>邮寄信息</h3>
+      <div>
+        <van-field v-if="status == 2" label="邮寄编码" placeholder="等待邮寄中..." readonly />
+        <van-field v-else label="邮寄编码" placeholder="0123455788999" readonly />
+      </div>
+    </div>
+
+
+    <div class="step-btn-group" :class="status != 1 ?'pt40':''">
+      <van-button v-if="status == 1" type="theme" class="btn">完成</van-button>
+      <van-button v-else type="theme" class="btn">返回课程</van-button>
     </div>
   </div>
 </template>
@@ -20,9 +36,7 @@
     data() {
       return {
         status: 1,
-        userInfo: {
-
-        }
+        userInfo: {}
       };
     },
     methods: {
@@ -33,7 +47,7 @@
 
 <style lang="less" scoped>
   .examstep_4-page {
-    padding: 25px 16px;
+    padding: 20px 16px;
 
     .info-form-block {
       width: 100%;
@@ -52,6 +66,7 @@
         font-size: 16px;
         font-weight: normal;
         margin: 0;
+        padding-bottom: 4px;
       }
 
       /deep/.van-cell {
@@ -79,6 +94,10 @@
     .step-btn-group {
       text-align: center;
       padding: 100px 0 0;
+
+      &.pt40 {
+        padding-top: 40px;
+      }
 
       .btn {
         width: 165px;
