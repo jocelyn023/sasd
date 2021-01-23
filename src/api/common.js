c46f1296f83541ea2648ddb017982829331d1e3c
刘巧艳 <961951661@qq.com>
2021-01-23 16:07:31 +0800

    代理商申请 未完成

--- src/api/common.js
+++ src/api/common.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { get, post } from '@/utils/request'
 
 // 字典查询
@@ -5,4 +6,26 @@ export const serchByKeyGroup = params => get('/wx/oapi/baseinfo/findDictionarysB
 
 // 图片上传
 export const getToken = params => post('/upload/getToken', params)
-export const uploadToQiniu = params => post(process.env.VUE_APP_UPLOAD_URL, params)
+/**
+ * 上传文件到阿里云
+ * @param url 阿里云文件上传地址
+ * @param parameter 上传参数
+ * @returns {AxiosPromise}
+ */
+export function uploadFileToQiniu(parameter, callback) {
+  var formData = new FormData();
+  formData.append("token", parameter.token);
+  formData.append("key", parameter.key);
+  formData.append("name", parameter.name);
+  formData.append("file", parameter.file);
+  return axios({
+    url: 'http://upload.qiniu.com/',
+    method: "post",
+    timeout: 0,
+    headers: { "Content-Type": "multipart/form-data" },
+    data: formData,
+    onUploadProgress: function(event) {
+      callback(event);
+    }
+  });
+}
