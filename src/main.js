bff618d5eed34e00e28317d2c1051465c5a3c2f6
刘巧艳 <961951661@qq.com>
2021-02-08 15:49:02 +0800

    分享

--- src/main.js
+++ src/main.js
@@ -5,6 +5,19 @@ import Vant from 'vant';
 import { Lazyload } from 'vant';
 import 'vant/lib/index.css';
 import 'lib-flexible/flexible'
+import { ShareImpl } from '@/utils/wxApi.js'
+
+let url = window.location.href
+// if(url.indexOf('from') != -1){
+//   url = window.location.href.split("?")[0];
+// }
+console.log(url + 'favicon.png');
+ShareImpl({
+  shareTitle: document.title,
+  shareDesc: document.title,
+  shareUrl: url,
+  shareImg: url + 'favicon.png',
+});
 
 Vue.use(Vant);
 Vue.use(Lazyload);
---
+++ src/utils/wxApi.js
@@ -0,0 +1,42 @@
+import wx from 'weixin-js-sdk';
+import { get, post } from '@/utils/request'
+const getWxConfig = params => post('/wx/config/load', params)
+
+const ShareImpl = function (option) {
+  //option是分享的配置内容*/
+  const url = window.location.href.split("#")[0];
+  console.log(url)
+  getWxConfig({ url: url }).then((response) => {
+    wx.config({
+      debug: false,
+      appId: response.data.appId,
+      timestamp: response.data.timestamp,
+      nonceStr: response.data.nonceStr,
+      signature: response.data.signature,
+      jsApiList: [
+        'updateAppMessageShareData',
+        'updateTimelineShareData',
+      ]
+    });
+  }).catch(error => {
+    window.console.log(error);
+  });
+  wx.ready(function () {
+    wx.updateAppMessageShareData({
+      title: option.shareTitle,
+      desc: option.shareDesc,
+      link: option.shareUrl,
+      imgUrl: option.shareImg
+    });
+    wx.updateTimelineShareData({
+      title: option.shareTitle,
+      desc: option.shareDesc,
+      link: option.shareUrl,
+      imgUrl: option.shareImg,
+    });
+  })
+}
+
+export {
+  ShareImpl
+}
